dff0db8dd009b0b90000033f70158068ebb076b1
子 <81725003+gdfsnhsw@users.noreply.github.com>
2021-05-31 15:37:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+set -e
+
+if [ -n "$EN_MODE_TUN" ]; then
+    #TUN模式
+    /usr/lib/clash/setup-redir-tun.sh &
+else
+    /usr/lib/clash/setup-tproxy.sh &
+fi
+
+# 开启转发，需要 privileged
+# Deprecated! 容器默认已开启
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
+
+if [ ! -e '/clash_config/dashboard' ]; then
+    echo "init /clash_config/dashboard"
+    cp -r /root/.config/clash/dashboard /clash_config/dashboard
+fi
+
+
+if [ ! -e '/clash_config/config.yaml' ]; then
+    echo "init /clash_config/config.yaml"
+    cp  /root/.config/clash/config.yaml /clash_config/config.yaml
+fi
+
+if [ ! -e '/clash_config/Country.mmdb' ]; then
+    echo "init /clash_config/Country.mmdb"
+    cp  /root/.config/clash/Country.mmdb /clash_config/Country.mmdb
+fi
+
+exec "$@"
