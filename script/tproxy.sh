777b2f1e604fb2f786ae2da0933ab5a7072be44d
子 <81725003+gdfsnhsw@users.noreply.github.com>
2021-05-31 16:41:29 +0800

    Update tproxy.sh

--- script/tproxy.sh
+++ script/tproxy.sh
@@ -17,7 +17,7 @@ function assert_command() {
 }
 
 function _setup(){
-    . /etc/default/clash
+    . /usr/lib/clash/common.sh
 
     ip route replace local default dev lo table "$IPROUTE2_TABLE_ID"
 
